47c44f0007fb27a53e64b2658c1db60183c5f184
Badi-Haki Martin <jojackblack@gmail.com>
2022-01-18 19:15:06 -0500

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+// no longer has main#main node
+const main = document.getElementById("main");
+main.remove();
+// has newheader variable that points to node 'h1#victory'
+// 'YOUR-NAME is the champion' string in h1#victory
+const newHeader=document.createElement("h1");
+newHeader.id="victory";
+newHeader.textContent="Haki is the champion!";
+document.append(newHeader);
